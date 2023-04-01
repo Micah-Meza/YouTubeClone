13d693ba5a9c18afe90e077414d54afeb8a572b8
Micah <airrenno@yahoo.com>
2023-04-01 00:29:31 -0500

    Updated views file with new get-by-video id + created a get all videos function + updated path for both new functions.

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_all_comments),
-    path('<str:videoId>/', views.posted_comments),
+    path('', views.get_all_videos),
+    path('<str:video_id>/', views.get_all_comments),
+    path('comments/<str:videoId>/', views.posted_comments),
 ]
+
+
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -5,17 +6,25 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
 
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def get_all_comments(request):
-    if request.method == 'GET':
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many = True)
-        return Response(serializer.data)
+def get_all_videos(request):
+    if request.method == "GET":
+            comments = Comment.objects.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data, status = status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        posted_comments(request)
-
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request, video_id):
+    
+    if request.method == "GET":
+        queryset = Comment.objects.all()
+        queryset = queryset.filter(video_id = video_id)
+        serializer = CommentSerializer(queryset, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -27,12 +36,4 @@ def posted_comments(request, videoId):
         serializer.is_valid(raise_exception = True)
         serializer.save(user = request.user)
         return Response(serializer.data, status = status.HTTP_201_CREATED)
-
-    elif request.method == 'GET':
-        comments = Comment.objects.filter(video_id = request.videoId)
-        serializer = CommentSerializer(comments, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-    
-
-
 
